ff447a84406dcf096f35071540569763d7d85b13
Prashanth Puranik <puranik@digdeeper.in>
2022-12-08 11:13:48 +0530

    Switch, handling not found, NavLink for avoiding page refresh (along with to and activeClassName)

--- demos-puranik3/04-react-project/workshops-app/src/components/App.tsx
+++ demos-puranik3/04-react-project/workshops-app/src/components/App.tsx
@@ -1,26 +1,32 @@
 // It was necessary to import React till React v17
 // import React from 'react';
-import { Route } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
-import Menu from './global/Menu/Menu';
-import Home from './pages/Home/Home';
-import WorkshopsList from './pages/WorkshopsList/WorkshopsList';
+import { Route, Switch } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Menu from "./global/Menu/Menu";
+import Home from "./pages/Home/Home";
+import WorkshopsList from "./pages/WorkshopsList/WorkshopsList";
+import PageNotFound from "./pages/PageNotFound/PageNotFound";
 
 function App() {
-  return (
-    <div>
-      <Menu />
+    return (
+        <div>
+            <Menu />
 
-      <Container className="my-4">
-        <Route path="/" exact>
-          <Home />
-        </Route>
-        <Route path="/workshops">
-          <WorkshopsList />
-        </Route>
-      </Container>
-    </div>
-  );
+            <Container className="my-4">
+                <Switch>
+                    <Route path="/workshops">
+                        <WorkshopsList />
+                    </Route>
+                    <Route path="/" exact>
+                        <Home />
+                    </Route>
+                    <Route path="**">
+                        <PageNotFound />
+                    </Route>
+                </Switch>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
--- demos-puranik3/04-react-project/workshops-app/src/components/global/Menu/Menu.tsx
+++ demos-puranik3/04-react-project/workshops-app/src/components/global/Menu/Menu.tsx
@@ -1,19 +1,20 @@
+import { NavLink } from 'react-router-dom';
 import { Navbar, Nav, Container } from "react-bootstrap";
 
 const Menu = () => {
     return (
         <Navbar bg="light" expand="lg">
             <Container fluid>
-                <Navbar.Brand href="/">Workshops App</Navbar.Brand>
+                <Navbar.Brand to="/" as={NavLink} activeClassName="active" exact>Workshops App</Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
                     <Nav
                         className="me-auto my-2 my-lg-0"
                         navbarScroll
                     >
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/workshops">List of workshops</Nav.Link>
-                        <Nav.Link href="/feedback">Feedback</Nav.Link>
+                        <Nav.Link to="/" as={NavLink} activeClassName="active" exact>Home</Nav.Link>
+                        <Nav.Link to="/workshops" as={NavLink} activeClassName="active" exact>List of workshops</Nav.Link>
+                        <Nav.Link to="/feedback" as={NavLink} activeClassName="active" exact>Feedback</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
