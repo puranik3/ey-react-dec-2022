2cf3e09a94f09749ff46b1dfeca6730c40005197
Prashanth Puranik <puranik@digdeeper.in>
2022-12-08 11:17:34 +0530

    formatted elements in menu

--- demos-puranik3/04-react-project/workshops-app/src/components/global/Menu/Menu.tsx
+++ demos-puranik3/04-react-project/workshops-app/src/components/global/Menu/Menu.tsx
@@ -5,16 +5,44 @@ const Menu = () => {
     return (
         <Navbar bg="light" expand="lg">
             <Container fluid>
-                <Navbar.Brand to="/" as={NavLink} activeClassName="active" exact>Workshops App</Navbar.Brand>
+                <Navbar.Brand
+                    as={NavLink}
+                    to="/"
+                    activeClassName="active"
+                    exact
+                >
+                    Workshops App
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
                     <Nav
                         className="me-auto my-2 my-lg-0"
                         navbarScroll
                     >
-                        <Nav.Link to="/" as={NavLink} activeClassName="active" exact>Home</Nav.Link>
-                        <Nav.Link to="/workshops" as={NavLink} activeClassName="active" exact>List of workshops</Nav.Link>
-                        <Nav.Link to="/feedback" as={NavLink} activeClassName="active" exact>Feedback</Nav.Link>
+                        <Nav.Link
+                            as={NavLink}
+                            to="/"
+                            activeClassName="active"
+                            exact
+                        >
+                            Home
+                        </Nav.Link>
+                        <Nav.Link
+                            as={NavLink}
+                            to="/workshops"
+                            activeClassName="active"
+                            exact
+                        >
+                            List of workshops
+                        </Nav.Link>
+                        <Nav.Link
+                            as={NavLink}
+                            to="/feedback"
+                            activeClassName="active"
+                            exact
+                        >
+                            Feedback
+                        </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
