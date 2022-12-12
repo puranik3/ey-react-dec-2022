83f459298671619615a2df34fea1cb937e517db7
Prashanth Puranik <puranik@digdeeper.in>
2022-12-12 09:13:50 +0530

    logger middleware

--- demos-puranik3/04-react-project/workshops-app/src/services/configureAxios.js
+++ demos-puranik3/04-react-project/workshops-app/src/services/configureAxios.js
@@ -7,3 +7,6 @@ axios.defaults.baseURL = REACT_APP_API_BASE_URL;
 axios.defaults.headers = {
     'Content-Type': 'application/json'
 };
+
+// to setup request and response interception
+// axios.interceptors
