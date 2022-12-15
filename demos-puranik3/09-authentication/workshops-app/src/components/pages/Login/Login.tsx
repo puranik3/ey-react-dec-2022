2c6e19fde1e1bd696cf05522b2c7dec21d33073e
Prashanth Puranik <puranik@digdeeper.in>
2022-12-15 08:30:58 +0530

    Able to login

--- demos-puranik3/09-authentication/workshops-app/src/components/pages/Login/Login.tsx
+++ demos-puranik3/09-authentication/workshops-app/src/components/pages/Login/Login.tsx
@@ -1,9 +1,12 @@
 import { useRef } from 'react';
-import { login } from '../../../services/auth';
+import { useHistory } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { handleSubmit } from '../../../utils/form';
+import { login } from '../../../services/auth';
 
 const Login = () => {
+    const { push } = useHistory();
+
     const emailRef = useRef<HTMLInputElement>( null );
     const passwordRef = useRef<HTMLInputElement>( null );
 
@@ -14,8 +17,8 @@ const Login = () => {
         };
 
         try {
-            const loginResponse = await login( credentials );
-            console.log( loginResponse );
+            await login( credentials );
+            push( '/workshops' );
         } catch( error ) {
             toast.error( (error as Error).message )
         }
@@ -24,10 +27,10 @@ const Login = () => {
     return (
         <form onSubmit={handleSubmit(doLogin)}>
             <div className='form-group my-2'>
-                <input type="email" placeholder="Your email" ref={emailRef} />
+                <input type="email" placeholder="Your email" ref={emailRef} defaultValue="john.doe@example.com" />
             </div>
             <div className='form-group my-2'>
-                <input type="password" ref={passwordRef} />
+                <input type="password" ref={passwordRef} defaultValue="Password123#" />
             </div>
             <button className='btn btn-primary'>Login</button>
         </form>
--- demos-puranik3/09-authentication/workshops-app/src/services/auth.ts
+++ demos-puranik3/09-authentication/workshops-app/src/services/auth.ts
@@ -2,8 +2,23 @@ import axios from 'axios';
 import ILoginRequest from '../models/ILoginRequest';
 import ILoginResponse from '../models/ILoginResponse';
 
+const KEY_TOKEN = 'token';
+const KEY_EMAIL = 'email';
+const KEY_ROLE = 'role';
+
 const login = async ( credentials : ILoginRequest ) => {
     const response = await axios.post( `/login`, credentials );
+
+    const {
+        email,
+        authToken,
+        role 
+    } = response.data;
+
+    localStorage.setItem( KEY_TOKEN, authToken );
+    localStorage.setItem( KEY_EMAIL, email );
+    localStorage.setItem( KEY_ROLE, role );
+
     return response.data as ILoginResponse;
 };
 
